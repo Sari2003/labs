948239a1f47efa36c06850735044ebe52b6a334c
sari2003 <s0556781624@gmail.com>
2024-05-09 17:01:26 +0300

    update

--- src/auction/auction.sol
+++ src/auction/auction.sol
@@ -7,30 +7,25 @@ import "./SafeMath.sol";
 
 contract Auction is ERC20, ERC721 {
     using SafeMath for uint256;
-
-    
     struct suggest{
 
         bool flag = false;
         uint amount;
 
     }
-
     address payable private owner;
     mapping(address => suggest) public suggestions;
     uint256 max;
-    uint start;
+    bool start=false;
     uint end;
     address[] public stack;
     uint256 public activationTime;//זמן ההפעלה של החוזה
     uint256 public constant SEVEN_DAYS = 7 days;// זמן קיום המכירה
 
-
-
      constructor() {
         owner = payable(msg.sender);
         end = block.timestamp() + 7;
-        start = block.timestamp;
+        start = true;
        
     }
     modifier onlyOwner(){
@@ -40,35 +35,37 @@ contract Auction is ERC20, ERC721 {
             _;
     }
 
-
     //הכנסת הצעה חדשה
     function addSuggest() public {
 
         require(start , "The auction doesnt start");
-        require(block.timestamp < end , "The Auction is over");
-        //הכנסת הצעה חדשה רק במקרה שההצעה הקודמת קטנה ממנה
-        require (suggestions(stack[stack.length]).amoumt < msg.sender, "You should offer a higher amount"){  
-            
-            //במקרה שהבן אדם כבר קיים
+        if(block.timestamp < end){
+            //הכנסת הצעה חדשה רק במקרה שההצעה הקודמת קטנה ממנה
+            require (suggestions[stack[stack.length]].amoumt < msg.sender, "You should offer a higher amount");
+                
+                //במקרה שהבן אדם כבר קיים
             if (suggestions[msg.sender].amoumt > 0 ){ 
 
-            uint lastSuggest = suggestions[msg.sender].amoumt;
-            require(msg.sender.balance >=msg.value , "you do not have enough money");
-            suggestions[msg.sender].amoumt = msg.value;
-            ERC20.transferFrom(msg.sender, address(this) , msg.value);
-            ERC20.transfer(msg.sender, lastSuggest);
-            stack.push(msg.sender);
-        }
-        else {//משתמש חדש
+                uint lastSuggest = suggestions[msg.sender].amoumt;
+                require(msg.sender.balance >=msg.value , "you do not have enough money");
+                suggestions[msg.sender].amoumt = msg.value;
+                ERC20.transferFrom(msg.sender, address(this) , msg.value);
+                ERC20.transfer(msg.sender, lastSuggest);
+                stack.push(msg.sender);
+            }
+            else {//משתמש חדש
 
-            require(msg.sender.balance >=msg.value , "you do not have enough money");
-            suggestions[msg.sender].amoumt= msg.value;
-            suggestions[msg.sender].flag = true;
-            ERC20.transferFrom(msg.sender, address(this) , msg.value);
-            stack.push(msg.sender);
-        }
-
+                require(msg.sender.balance >=msg.value , "you do not have enough money");
+                suggestions[msg.sender].amoumt= msg.value;
+                suggestions[msg.sender].flag = true;
+                ERC20.transferFrom(msg.sender, address(this) , msg.value);
+                stack.push(msg.sender);
+            }
        }
+       else{
+            start=false;
+            endAuction();
+        }
 
     }
     //הסרת הצעה
@@ -84,21 +81,16 @@ contract Auction is ERC20, ERC721 {
     }
 
 
-
-    
-    // פונקציה זו מפעילה את החוזה ומקבעת את זמן ההפעלה
-    function activateContract() external {
-        activationTime = block.timestamp;
-    }
-
-    // פונקציה זו מאפשרת לבדוק האם עברו כבר שבעה ימים מאז הפעלת החוזה
-    function isSevenDaysPassed() public view returns (bool) {
-        return block.timestamp >= activationTime.add(SEVEN_DAYS);
-    }
-
     // פונקציה שמתבצעת רק כאשר עוברים שבעה ימים מהפעלת החוזה
-    function myFunction() external {
-        require(isSevenDaysPassed(), "Seven days have not passed yet.");
-        // הכנס כאן את הפעולות שברצונך לבצע
+    function endAuction() external onlyOwner{
+
+        uint i= stack.length; 
+        for(;i>=0 ; && suggestions[stack[i]].flag == false; i--);
+        ERC721.transfer(suggestions[stack[i]], 1);
+        for(uint j= i-1; j>=0; j--){
+            if(suggestions[stack[i]].flag)
+                ERC20.transfer(suggestions[stack[j]], suggestions[stack[j]].amoumt);
+        }
+
     }
 }
