3629ab527ba00a3d9d5bfc36a2c71414039ff6f2
Sari Homri - Zcharya <121660734+Sari2003@users.noreply.github.com>
2024-06-03 15:00:09 +0300

    Update AMM.sol

--- src/AMM/AMM.sol
+++ src/AMM/AMM.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "./my_tokenA.sol";
-import "./my_tokenB.sol";
+import "./MyTokenA.sol";
+import "./MyTokenB.sol";
 
 pragma solidity ^0.8.20;
 
