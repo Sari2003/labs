1589e67b7c4d1f6602a20344cf740705be0aa7ed
Sari Homri - Zcharya <121660734+Sari2003@users.noreply.github.com>
2024-06-03 15:04:40 +0300

    Update nft.sol

--- src/auction/nft.sol
+++ src/auction/nft.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 //import "@openzeppelin/contracts/access/Ownable.sol";
 contract MyERC721 is ERC721 {
-    constructor(string memory name, string memory symbol) ERC721(TokenNFT, NFT) {}
+    constructor(string memory name, string memory symbol) ERC721("TokenNFT", "NFT") {}
     function mint(address to, uint256 tokenId) public {
         _mint(to, tokenId);
     }
