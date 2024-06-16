406c486da724f96acdf64ebbacc4f2063eccef08
sari2003 <s0556781624@gmail.com>
2024-06-16 15:57:30 +0300

    up

--- lerningSolidity/dataLocations.sol
+++ lerningSolidity/dataLocations.sol
@@ -4,11 +4,34 @@ pragma solidity ^0.8.20;
 contract DataLocations{
 
     uint256[] public arr;
-    mapping(uint256 => address) map;
+
+    mapping(uint => address) map;
     struct MyStruct{
        
        uint num;
     }
     mapping(address=>MyStruct) myStructs;
-    
+   
+    function f1()public{
+              
+      f2(myStructs[1],arr, map);
+      MyStruct storage myS=myStructs[1];
+      MyStruct memory mySM=myStructs(1);
+
+    }
+
+    function f2(MyStruct storage _myStructs ,uint [] storage _arr, mapping(uint=>address)storage _map)internal{
+
+        //...
+    }
+    function g(uint256[] memory _arr) public returns (uint256[] memory) {
+        //...
+    }
+
+    function h(uint256[] calldata _arr) external {
+        // ...
+    }
+   
+
+
 }
